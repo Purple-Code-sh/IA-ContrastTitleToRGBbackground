3ed8480b6d3305d097e7209a5d9e9f7db8806be3
Purple-Code-sh <purple.code.sh@gmail.com>
2023-06-25 23:48:37 -0600

    returning to logic into html

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-import "./jscolor.js";
 
-const input = document.getElementById("colorInput");
-console.log(input);
-
-input.oninput(update(input, "pr4"));
-
-function update(picker, selector) {
-  console.log(picker.jscolor);
-  document.getElementById(selector).style.background =
-    picker.jscolor.toBackground();
-}
-jscolor.trigger("input change");
